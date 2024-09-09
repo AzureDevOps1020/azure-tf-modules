45219b1af68dd823ea1cafa5306f740d71ba10aa
Ngachandra <mydevops730830@outlook.com>
2024-09-09 13:58:45 +0530

    updated code

--- modules/322. azurerm_lb_rule.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_lb_rule" "example" {
-  name                = "example-lb-rule"
-  resource_group_name = azurerm_resource_group.example.name
-  loadbalancer_id     = azurerm_lb.example.id
-  protocol            = "Tcp"
-  frontend_port       = 80
-  backend_port        = 80
-  enable_floating_ip  = false
-}
