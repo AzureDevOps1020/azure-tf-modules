b13e0a41564c34dd713bbd80980583314f87332e
Ngachandra <mydevops730830@outlook.com>
2024-09-09 19:10:32 +0530

    updated workflow file in dev

--- azure-virtual-machine/modules/azurerm_network_security_group/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_network_security_group" "example" {
-  name                = var.nsg_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
--- azure-virtual-machine/modules/azurerm_network_security_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nsg_id" {
-  description = "The ID of the network security group."
-  value       = azurerm_network_security_group.example.id
-}
-
-output "nsg_name" {
-  description = "The name of the network security group."
-  value       = azurerm_network_security_group.example.name
-}
